da50e505484774183b8eb35b843a6c7c0826ca1b
Pratham <chudasamapratham142@gmail.com>
2025-08-24 13:47:40 +0530

    feat: Add favicon link to the root layout for improved branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head>
+				<link rel="icon" href="/assets/logo.svg" />
 				{/* Google Tag Manager */}
 				<Script id="gtm" strategy="afterInteractive">
 					{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
